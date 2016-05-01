b412b7cb40eecd4aeadb2b063686c9bf9ac9d8a1
Shusui <wir0.gongasgreen@gmail.com>
2016-05-01 19:21:38 +0100

    Added script to create dataset.csv from new datasets

---
+++ server/gen_db.py
@@ -0,0 +1,30 @@
+import os
+
+def parse_ctg(ctg):
+	if '.' in ctg:
+		ctg = ctg[ctg.find('.') + 1:]
+	if '-101' in ctg:
+		ctg = ctg[:ctg.find('-101')]
+	if '_' in ctg:
+		ctg = '-'.join(ctg.split('_'))
+	return ctg
+
+def gen_db():
+	f = open('dataset.csv', 'w+')
+	img_count = 0
+
+	dir_list = next(os.walk('.'))[1]
+	os.mkdir('.images')
+	for db in dir_list:
+		ctg_list = next(os.walk(db + '/.'))[1]
+		for c in ctg_list[:2]:
+			ctg = parse_ctg(c)
+			for img in os.listdir(db + '/' + c):
+				if '.jpg' in img:
+					os.system('cp ' + db + '/' + c + '/' + img + ' .images/' + str(img_count) + '.jpg')
+					f.write(str(img_count) + '.jpg' + ',' + c + '\n')
+					img_count += 1
+	f.close()
+
+if __name__ == '__main__':
+	gen_db()
